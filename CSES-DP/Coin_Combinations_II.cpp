25ed3c634872f4ecbb96aea35c831eb848537da9
Ritwik <sritwik03@gmail.com>
2022-07-07 19:46:11 +0530

    added code

---
+++ CSES-DP/Coin_Combinations_II.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define mp make_pair
+#define pb push_back
+#define INF (int)1e9
+#define rep(i, s, n) for(int i=s; i<n; i++)
+#define rrep(i, n, s) for(int i=n; i>=s; i--)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef set<int> si;
+const int mod = 1e9 + 7;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+
+
+
+
+void Solve()
+{
+    int n, x;
+    cin >> n >> x;
+    vi a(n);
+    rep(i, 0, n) cin >> a[i];
+
+    vvi dp(n + 1, vi(x + 1));
+    dp[0][0] = 1;
+    debug(dp);
+    rep(i, 1, n + 1)
+    rep(j, 0, x + 1)
+    {
+        dp[i][j] = dp[i - 1][j];
+        if (j - a[i - 1] >= 0)
+            dp[i][j] = (dp[i][j] + dp[i][j - a[i - 1]]) % mod;
+    }
+    cout << dp[n][x] % mod << '\n';
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+    auto start = chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // int t;
+    // cin >> t;
+    // while (t--)
+    {
+        Solve();
+    }
+    auto end = chrono::high_resolution_clock::now();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+    time_taken *= 1e-9;
+    cerr << "\n\nTime : " << fixed
+         << time_taken << setprecision(9);
+    cerr << " sec" << endl;
+    return 0;
+}
--- CSES-DP/Readme.md
+++ CSES-DP/Readme.md
@@ -23,9 +23,43 @@ Like in "Minimizing Coins", we loop over the possibilities for last coin added. 
 ### Time Complexity : ***O(n . x)***   
 <br>
 
+# [Coin Combinations II](https://cses.fi/problemset/task/1636)
+
+### `dp[i][j] = # of ways to make sum j using the first i coins`
+Initially, we say we have dp[0][0] = 1, i.e we have the empty set with sum zero.
+
+When calculating dp[i][j], we consider the i'th coin. Either we didn't pick the coin, then there are dp[i-1][j] possibilities. 
+Otherwise, we picked the coin. Since we are allowed to pick it again, there are dp[i][j-a[i]] possibilities (not dp[i-1][j-a[i]] possibilities).
+
+Because we consider the coins in order, we will only count one order of coins.
+### Time Complexity : ***O(n . x)***   
+<br>
+
 # [Removing Digits](https://cses.fi/problemset/task/1637)
 
 ### `dp[i] = minimum # of operations to go from i to 0`
 When considering a number i, for each digit in the decimal representation of i, we can try to remove it. The transition is therefore: dp[i] =  min~d∈digits(i)~ dp[i-d]. 
 We initialize dp[0] = 0.
 ### Time Complexity : ***O(n)***   
+<br>
+
+# [Grid Paths](https://cses.fi/problemset/task/1638)
+
+### `dp[i][j] = # of ways to reach row i and col j`
+We say there is one way to reach (0,0), dp[0][0] = 1.
+
+When we are at some position with `a[i] = .`, we came either from the left or top. So the number of ways to get to there is the number of ways to get to the position above, plus the number of ways to get to the position to the left. 
+We also need to make sure that the number of ways to get to any position with `a[i] = #` is 0.
+### Time Complexity : ***O(n^2^)***   
+<br>
+
+# [Book Shop](https://cses.fi/problemset/task/1158)
+
+### `dp[i][j] =  maximum # of pages we can get for price at most j, only buying among the first i books`
+Initially dp[0][j] = 0 for all j, as we can't get any pages without any books.
+
+When calculating dp[i][j], we look at the last considered book, the i'th book. We either didn't buy it, leaving j money for the first i-1 books, giving dp[i-1][j] pages. 
+Or we bought it, leaving j-price[i-1] money for the other i-1 books, and giving pages[i-1] extra pages from the bought book. 
+Thus, buying the i'th book gives dp[i-1][j-price[i-1]] + pages[i-1] pages.
+### Time Complexity : ***O(n . x)***   
+<br>
